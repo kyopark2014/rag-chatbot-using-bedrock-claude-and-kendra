d2bc490f55d38c437a5e11fc283126faf7b7df45
ksdyb.park <ksdyb@amazon.com>
2023-11-28 16:47:29 +0900

    update

--- html/chat.js
+++ html/chat.js
@@ -1,4 +1,4 @@
-const endpoint = 'wss://l0f3oo32fe.execute-api.us-west-2.amazonaws.com/dev';
+const endpoint = 'wss://g51jvs4r48.execute-api.ap-northeast-1.amazonaws.com/dev';
 const langstate = 'korean'; // korean or english
 let webSocket
 let isConnected = false;
